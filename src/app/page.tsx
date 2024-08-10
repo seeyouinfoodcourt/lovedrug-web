f2f79263f5fb8964e7dc28b1f6fe47275fff9652
Mikkel <lovedrugfb@gmail.com>
2024-08-10 12:05:07 +0200

    responsive image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
+import logo from '../../public/img/lovedrug-logo-white.png';
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <Image
-        src={'/img/lovedrug-logo-white.png'}
-        width={500}
-        height={80}
+        src={logo}
+        sizes="100vw"
+        style={{ width: '40%', height: 'auto' }}
         alt="Lovedrug Logo"
       />
     </div>
