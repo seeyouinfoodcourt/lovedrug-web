6898634b61553b238353794a741e973bccf6bbc5
Mikkel <lovedrugfb@gmail.com>
2024-08-16 14:03:55 +0200

    removed products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,6 @@ import { Button } from '@/components/ui/button';
 import { createClient } from '@/lib/supabase';
 
 export default async function Home() {
-  const supabase = createClient();
-  const { data: products } = await supabase.from('products').select();
-
-  console.log('products', products);
-
-  return <pre>{JSON.stringify(products, null, 2)}</pre>;
   return (
     <>
       {/* <div className="bg-gray-400 w-full h-screen flex items-center justify-center">
